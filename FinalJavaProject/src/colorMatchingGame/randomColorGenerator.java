a30cb6323203143cefac1064661ec3d49be71ccc
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-10-09 08:41:46 -0500

    save

--- FinalJavaProject/src/colorMatchingGame/randomColorGenerator.java
+++ FinalJavaProject/src/colorMatchingGame/randomColorGenerator.java
@@ -4,19 +4,19 @@ public class randomColorGenerator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String color = null;
+		String [] randomColors = new String [5];
 		String colors[] = {"Blue", "Red", "Green", "Yellow", "Black", "White"};
 		
 		for(int x = 0; x < colors.length; x ++)
 		{
 			int randomNumber = (1 + (int)(Math.random() * 6) - 1);
-			color = colors[randomNumber];
+			randomColors[x] = colors[randomNumber];
 		}
-		System.out.print(color + " ");
-		System.out.print(color + " ");
-		System.out.print(color + " ");
-		System.out.print(color + " ");
-		System.out.print(color + " ");
+		System.out.print(randomColors[0] + " ");
+		System.out.print(randomColors[1] + " ");
+		System.out.print(randomColors[2] + " ");
+		System.out.print(randomColors[3] + " ");
+		System.out.print(randomColors[4] + " ");
 		
 	}
 
